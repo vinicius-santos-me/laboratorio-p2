e9d477c55bea9c9a929a8b2b697cd8031740c07d
root <root@DESKTOP-2KCKVE0.localdomain>
2026-04-30 23:12:56 -0300

    questões dirlididi

---
+++ dirlididi/PalavrasIngratas.java
@@ -0,0 +1,41 @@
+/**
+ * Laboratório de Programação 2 - Lab 1
+ * * @author Vinicius Santos Sousa - 20250030551
+ */
+import java.util.*;
+
+public class PalavrasIngratas {
+	public static void main (String args[]) {
+		Scanner sc = new Scanner(System.in);
+		PalavrasIngratas funcoes = new PalavrasIngratas();
+		int contador = 0;
+		while(contador != 3){
+		boolean ingrata = true;
+		String palavra = sc.nextLine();
+		for(String letra : palavra.split("")) {
+			if(funcoes.MeuIn(letra, "aeiouAEIOU")){
+				ingrata = false;
+				break;
+			}
+		}
+		if(ingrata) {
+			System.out.println(palavra);
+			contador++;
+			}
+		}
+		sc.close();
+	}
+
+
+	public boolean MeuIn(String letra, String vogais){
+		String[] vogaisList = vogais.split("");
+		for(int i = 0; i < vogaisList.length; i++) {
+			if(vogaisList[i].equals(letra)){
+				return true;
+			}
+		}
+		return false;
+	}
+	
+}
+
---
+++ dirlididi/Temperatura.java
@@ -0,0 +1,22 @@
+/**
+ * Laboratório de Programação 2 - Lab 1
+ * * @author Vinicius Santos Sousa - 20250030551
+ */
+import java.util.*;
+
+public class Temperatura {
+	public static void main (String args[]){
+	Scanner sc = new Scanner(System.in);
+	int total = 0;
+	while(true) {
+		int numero = sc.nextInt();
+		if(numero < 0) {
+			total++;
+			}else if(numero == 0) {
+				break;
+			}
+		}
+	System.out.println(total);
+	}
+}
+
